5bc91ad123f1347ea8a39ff2d086da7582952e53
AKAVespaET3 <quattrini244@gmail.com>
2023-05-31 13:14:25 +0900

    Vaule Middle

--- MaxMins.java
+++ MaxMins.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class MaxMins {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        try {
+            Scanner in = new Scanner(System.in);
 
-        System.out.print("A값을 입력하세요 : " );
-        int A = in.nextInt();
-        System.out.print("B값을 입력하세요 : " );
-        int B = in.nextInt();
-
-        int Middle = (A + B)/2 ;
-
-        System.out.print("중간값은 " + Middle + " 입니다." );
+            System.out.print("A값을 입력하세요 : " ); 
+            int A = in.nextInt(); //A값 받기
+            System.out.print("B값을 입력하세요 : " );
+            int B = in.nextInt(); //B값 받기
+    
+            int Middle = (A + B)/2 ; //중간값 펑션
+    
+            System.out.print("중간값은 " + Middle + " 입니다." ); 
         
-
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+        
     }
 }
