a9e7228bbea97dabe3aa7675a1ef033370028d3c
kimskkkkky <ses513a@gmail.com>
2023-05-31 13:09:29 +0900

    min값 찾기_SKY

--- MaxMins.java
+++ MaxMins.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class MaxMins {
     public static void main(String[] args) {
         try {
-            Scanner in = new Scanner(System.in);
+            Scanner myObj = new Scanner(System.in); //스캔과 변수는 SKY 코드
 
-            System.out.print("A값을 입력하세요 : " ); 
-            int A = in.nextInt(); //A값 받기
-            System.out.print("B값을 입력하세요 : " );
-            int B = in.nextInt(); //B값 받기
+            System.out.println("number1을 입력하세요."); //스캐너로 number1값 입력받기
+            int number1 = myObj.nextInt();
+
+            System.out.println("number2을 입력하세요."); //스캐너로 number2값 입력받기
+            int number2 = myObj.nextInt();
+
+            if (number1 > number2) {  //min값 구하기
+                System.out.println("최소값은" + number2 + "입니다.");
+            } else {
+                System.out.println("최소값은" + number1 + "입니다.");
+            }
     
-            int Middle = (A + B)/2 ; //중간값 펑션
+            int Middle = (number1 + number2)/2 ; //Goni의 입력받는 변수 A,B를 SKY가 number1,2로 바꿈
     
-            System.out.print("중간값은 " + Middle + " 입니다." ); 
+            System.out.print("중간값은 " + Middle + " 입니다." ); //Goni의 코드 middle값 구하기
         
         } catch (Exception e) {
             // TODO: handle exception
         }
         // return 0;
         
+
+
     }
 }
