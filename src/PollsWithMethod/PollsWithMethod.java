d36d70924ea14934dceaaab70226c80b72630e11
ppquokka <quokka2335@gmail.com>
2023-06-13 17:54:19 +0900

    230613  SQL팀플_최종 제출 파일양식(ing) 및 화면정의서

--- src/PollsWithMethod/PollsWithMethod.java
+++ src/PollsWithMethod/PollsWithMethod.java
@@ -1,5 +1,14 @@
 package PollsWithMethod;
 
 public class PollsWithMethod {
-
+public static void main(String[] args) {
+    try {
+        // - MySQL workbench 실행 : JDBC
+        // - user, password와 접속  IP:port 접속
+        String url = "";
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+}
 }
