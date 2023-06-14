0de37563a8f77c6808ad57d0a94f3036f63551f3
ppquokka <quokka2335@gmail.com>
2023-06-14 11:28:16 +0900

    terminalpolls 생성

---
+++ src/PollsWithMethod/terminalpolls/PollList.java
@@ -0,0 +1,11 @@
+package PollsWithMethod.terminalpolls;
+
+public class PollList {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
